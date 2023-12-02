6f023c5fe179818b57e5a6a0f5e64e174a31e805
Hummel009 <hummel.turbamentum@gmail.com>
2023-12-02 16:32:13 +0300

    Update Utils.kt

--- src/main/kotlin/hummel/Utils.kt
+++ src/main/kotlin/hummel/Utils.kt
@@ -7,7 +7,7 @@ const val user: String = "root"
 const val password: String = "amogus134"
 
 fun addSampleValue(connection: Connection, tableName: String, sampleValue: Int) {
-	val query = "INSERT INTO $tableName (${tableName.substring(0, tableName.length - 1)}) VALUES (?)"
+	val query = "INSERT INTO $tableName (${tableName.dropLast(1)}) VALUES (?)"
 	connection.prepareStatement(query).use {
 		it.setInt(1, sampleValue)
 		it.executeUpdate()
