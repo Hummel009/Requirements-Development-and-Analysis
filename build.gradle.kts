d97e3c2023aefeed8e89c1fb4bc6f7665afef396
Hummel009 <hummel.turbamentum@gmail.com>
2024-07-24 17:38:29 +0300

    latest.release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "2.0.0"
-	id("io.ktor.plugin") version "2.3.11"
+	id("org.jetbrains.kotlin.jvm") version "latest.release"
+	id("io.ktor.plugin") version "latest.release"
 }
 
 group = "com.github.hummel"
 version = "1.0-SNAPSHOT"
 
 dependencies {
-	implementation("com.mysql:mysql-connector-j:8.4.0")
-	implementation("com.rabbitmq:amqp-client:5.21.0")
+	implementation("com.mysql:mysql-connector-j:latest.release")
+	implementation("com.rabbitmq:amqp-client:latest.release")
 
 	implementation("io.ktor:ktor-server-core-jvm")
 	implementation("io.ktor:ktor-server-freemarker-jvm")
